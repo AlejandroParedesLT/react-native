e8bb28ab4f94141bbfdda90c2b456d6ff37f886f
Alejandro Paredes <alejocangrejo10@gmail.com>
2022-12-21 11:31:34 -0400

    Deleted files that werent working in deployment

--- 07-Pokedex/App.tsx
+++ 07-Pokedex/App.tsx
@@ -1,14 +1,14 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react'
-import { Text, View } from 'react-native';
+import React from 'react';
+//import { Text, View } from 'react-native';
 //import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Navigator } from './src/navigator/Navigator';
 
 export const App = () => {
-  return (  
+  return (
     <NavigationContainer>
       <Navigator />
     </NavigationContainer>
-  )
-}
+  );
+};
--- Pokedex/src/screens/PokemonScreen.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const PokemonScreen = () => {
-  return (
-    <div>PokemonScreen</div>
-  )
-}
